2d5dfe6492e1accf1a3371898d73c23297535a50
Moungjung Kim <moungjung@gmail.com>
2019-09-18 19:19:43 -0600

    Did Add, Update, Delete

--- dmit2018E01-master/ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ dmit2018E01-master/ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -12,38 +12,84 @@ using System.ComponentModel;
 
 namespace ChinookSystem.BLL
 {
-        [DataObject]
-        public class AlbumController
+    [DataObject]
+    public class AlbumController
+    {
+        #region Queries
+        public List<Album> Album_List()
         {
-            public List<Album> Album_List()
+            using (var context = new ChinookContext())
             {
-                using (var context = new ChinookContext())
-                {
-                    return context.Albums.ToList();
-                }
-            }
-
-            public Album Album_Get(int albumid)
-            {
-                using (var context = new ChinookContext())
-                {
-                    return context.Albums.Find(albumid);
-                }
-            }
-
-            [DataObjectMethod(DataObjectMethodType.Select,false)]
-            public List<Album> Album_FindByArtist(int artistid)
-            {
-                using (var context = new ChinookContext())
-                {
-                    var results = from x in context.Albums
-                                  where x.ArtistId == artistid
-                                  select x;
-
-                
-                    return results.ToList();
-                }
+                return context.Albums.ToList();
             }
         }
-    
+
+        public Album Album_Get(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> Album_FindByArtist(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.ArtistId == artistid
+                              select x;
+
+
+                return results.ToList();
+            }
+        }
+        #endregion
+
+
+
+        #region ADD, Update, Delete
+        public int Album_Add(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Albums.Add(item); //staging
+                context.SaveChanges();   //Commit
+                return item.AlbumId;  //return new id value
+            }
+        }
+
+        public int Album_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Entry(item).State =
+                    System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+
+        public int Album_Delete(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                if (existing == null)
+                {
+                    throw new Exception("Album not on file. Delete unnecessary");
+                }
+                else
+                {
+                    context.Albums.Remove(existing);
+                    return context.SaveChanges();
+                }
+
+
+            }
+        }
+
+        #endregion
+
+    }
 }
